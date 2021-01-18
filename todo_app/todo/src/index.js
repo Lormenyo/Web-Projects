84a0b0ef78b0eab734b47a960767dcbc1cbd7a11
Lormenyo <lormenyoh@gmail.com>
2021-01-18 11:07:00 +0000

    managing state at square level

--- todo_app/todo/src/index.js
+++ todo_app/todo/src/index.js
@@ -5,10 +5,19 @@ import "./index.css"
 
 
 class Square extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            value:null,
+        };
+    }
+
     render(){
         return (
-            <button className="square">
-                {this.props.value}
+            <button 
+                className="square" 
+                onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
@@ -16,6 +25,13 @@ class Square extends React.Component{
 
 
 class Board extends React.Component {
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         squares: Array(9).fill(null)
+    //     }
+    // }
+
     renderSquare(i){
         return <Square value={i}/>;
     }
