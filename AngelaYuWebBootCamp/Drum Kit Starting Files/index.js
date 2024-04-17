490a58972bffff03caa45baa11dbfa92d3ed435f
Hannah Lormenyo <lormenyoh@gmail.com>
2024-04-17 21:25:15 +0100

    completed drum kit

--- AngelaYuWebBootCamp/Drum Kit Starting Files/index.js
+++ AngelaYuWebBootCamp/Drum Kit Starting Files/index.js
@@ -4,7 +4,44 @@ for (let index = 0; index < drums.length; index++) {
     const element = drums[index].addEventListener("click", playSound);  
 }
 
+document.addEventListener("keydown", function (event) {
+    getSound(event.key);
+})
+
 function playSound() {
-    let audio = new Audio("./sounds/tom-1.mp3");
+    let drumInnerHTML = this.innerHTML;
+    getSound(drumInnerHTML);
+}
+
+function playAudio(path) {
+    let audio = new Audio(path);
     audio.play();
+}
+
+function getSound(drum) {
+    switch (drum) {
+        case "w":
+            playAudio("./sounds/tom-1.mp3");
+            break;
+        case "a":
+            playAudio("./sounds/tom-2.mp3");
+            break; 
+        case "s":
+            playAudio("./sounds/tom-3.mp3");
+            break;  
+        case "d":
+            playAudio("./sounds/tom-4.mp3");
+            break;
+        case "j":
+            playAudio("./sounds/snare.mp3");
+            break;
+        case "k":
+            playAudio("./sounds/kick-bass.mp3");       
+            break;
+        case "l":
+            playAudio("./sounds/crash.mp3");
+            break;
+        default:
+            break;
+    }
 }
