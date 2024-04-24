4e35cdb98b21c5ab95713bb369719c9e354743ea
Hannah Lormenyo <lormenyoh@gmail.com>
2024-04-24 20:34:14 +0100

    updated middleware

--- AngelaYuWebBootCamp/3.4 Middleware/index3.js
+++ AngelaYuWebBootCamp/3.4 Middleware/index3.js
@@ -3,6 +3,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next) {
+  console.log(`Request ${req.method} ${req.url} ${res.statusCode}`);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
--- AngelaYuWebBootCamp/3.4 Middleware/index4.js
+++ AngelaYuWebBootCamp/3.4 Middleware/index4.js
@@ -1,7 +1,32 @@
 import express from "express";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const _dirname = dirname(fileURLToPath(import.meta.url));
+let bandName = "";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+function generateBandName(req, res, next) {
+  bandName = `${req.body.street}${req.body.pet}`;
+  next();
+}
+
+app.use(generateBandName);
+
+app.get("/", (req, res)=>{
+  res.sendFile(_dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res)=>{
+  let pageHTML = `<h1>Your band name is </h1> </br> <h2> ${bandName}</h2>`;
+  res.send(pageHTML);
+});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
