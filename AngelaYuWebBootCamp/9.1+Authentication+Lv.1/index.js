3d1403a6a2205cefdf9037a6261d7bb26f45a65c
Hannah Lormenyo <lormenyoh@gmail.com>
2024-05-19 04:40:54 +0100

    basic auth

--- AngelaYuWebBootCamp/9.1+Authentication+Lv.1/index.js
+++ AngelaYuWebBootCamp/9.1+Authentication+Lv.1/index.js
@@ -12,6 +12,8 @@ const db = new pg.Client({
   port: 5432,
 });
 
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -31,11 +33,13 @@ app.post("/register", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
 
+  console.log(email, password);
+
   try {
     const checkIfUserExists = await db.query("SELECT * FROM users WHERE email = $1", [email]);
 
     if (checkIfUserExists.rows.length > 0){
-      res.send("User already exists. Try logging in!");
+      res.send("Email already exists. Try logging in!");
     }else{
       const results = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, password]);
       console.log(results);
@@ -55,7 +59,6 @@ app.post("/login", async (req, res) => {
 
     if (checkIfUserExists.rows.length > 0){
       const storedPassword = checkIfUserExists.rows[0].password;
-
 
       if (storedPassword == password) {
         res.render("secrets.ejs");
